36b1b05d2b485a2b6ccfe6e5abeb87ddb54561b9
fernandoquinellato <42873800+fernandoquinellato@users.noreply.github.com>
2018-09-27 09:17:47 -0300

    Create cafe3.c

---
+++ cafe3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+     int ai, af, idade;
+     printf("\n Informe seu ano de nascimento");
+     scanf("%d", &ai);
+     af=2018;
+     idade=af-ai;
+     printf("\nIdade=%d", idade);
+     system("pause");
+     return 0;
+}
