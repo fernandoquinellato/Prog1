f4f556fcdd522343f963d3b040fd9a67b253c24c
fernandoquinellato <42873800+fernandoquinellato@users.noreply.github.com>
2018-09-27 09:16:21 -0300

    Create cafe1.c

---
+++ cafe1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+          int a=0, cont;
+          
+          if(a<1)
+           {
+            for(cont=1;cont<=100;cont++)
+            printf("\n%i", cont);
+           }
+       system("pause");
+       return 0;
+}
